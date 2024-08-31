f41604a5b8e92ec52bb381877d45cb54ffab6404
niedexi <niedexi10@gmail.com>
2024-08-31 19:38:07 +0800

    fix user api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const user = require("./routes/api/user");
-const order = require("./routes/api/order");
+// const order = require("./routes/api/order");
 
 const app = express();
 
@@ -16,22 +16,18 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 // Connect to DB
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+    .connect(db)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  });
+});
 
 // Routes
 app.use("/user", user);
-app.use("/order", order);
+// app.use("/order", order);
 
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Server running on port ${port}`));
