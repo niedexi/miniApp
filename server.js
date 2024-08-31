993a5ff2c1d86eac10875f8ed70e36d3aaa003a7
niedexi <niedexi10@gmail.com>
2024-08-31 13:21:42 +0800

    change port

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get('/test', (req, res) => {
     res.send('Hello World!')
   })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`Server running on port ${port}`));
