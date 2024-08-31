753f44302c7ce053900bd033d9ac9e2c8c88fe8c
niedexi <niedexi10@gmail.com>
2024-08-31 13:30:54 +0800

    change route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/test', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
